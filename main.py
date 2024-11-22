7fec807d5356b1cb98952a6480f2e3e048f5cdbe
njjorgensen <nj.jorg@gmail.com>
2024-11-22 21:31:33 +1000

    added player shape

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting asteroids!")
 
     pygame.init()
+    clk = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
 
     while True:
@@ -13,7 +18,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clk.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
